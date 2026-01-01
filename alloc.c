fc961d34d0a3c33ff97f0411394a1c610de6ad77
sora <zeluis30012007@gmail.com>
2026-01-01 12:28:09 +0000

    alloc working, needs merge ańd realloc maybe

--- alloc.c
+++ alloc.c
@@ -5,11 +5,13 @@
 #define HEAP_INIT_SIZE (size_t) 4096
 #define BLK_STRUCT_SIZE (size_t) sizeof(struct heap_blk)
 #define PAYLOAD(blk) (void *)((char *)blk + BLK_STRUCT_SIZE)
+#define BLK_HEADER(payload) (struct heap_blk *)((char *)payload - BLK_STRUCT_SIZE)
 
 struct heap_blk *head = NULL;
 
 void print_blk() {
     struct heap_blk *curr = head;
+    size_t cnt = 0;
     while (curr){  
         void *payload = PAYLOAD(curr);
         printf("BLOCK POINTER: %p\n", (void *) curr);
@@ -18,8 +20,10 @@ void print_blk() {
         printf("BLOCK PREV: %p\n", (void *) curr->prev);
         printf("BLOCK STATUS: %d\n", (int) curr->status);
         printf("BLOCK CONTENTS: %p\n\n\n",payload);
+        cnt += curr->size;
         curr = curr->next;
     }
+    printf("TOTAL HEAP SIZE: %zu\n\n",cnt);
 }
 
 void init_heap() { 
@@ -29,7 +33,7 @@ void init_heap() {
         return;
     }
     head = (struct heap_blk *) start_blk;
-    head->size = HEAP_INIT_SIZE - BLK_STRUCT_SIZE;
+    head->size = HEAP_INIT_SIZE;
     head->next = NULL;
     head->prev = NULL;
     head->status = FREE;
@@ -37,13 +41,25 @@ void init_heap() {
 
 struct heap_blk *split_chunk(struct heap_blk *split_blk, size_t size) {
     struct heap_blk *blk = split_blk;
-    split_blk->size -= size;  
-    split_blk->prev = blk;
+
+    size_t sp_size = split_blk->size;
+    struct heap_blk *sp_next = split_blk->next;
+    struct heap_blk *sp_prev = split_blk->prev;
+
     split_blk = (void *)((char *)split_blk + size);
-    blk->prev = split_blk->prev;
+    split_blk->size = sp_size - size;  
+    split_blk->prev = blk;
+    split_blk->next = sp_next;
+    split_blk->status = FREE;
+
+    if (sp_prev)
+        sp_prev->next = blk;
+    blk->prev = sp_prev;
     blk->next = split_blk;
     blk->size = size;
     blk->status = ALLOC;
+    if (head == split_blk)
+        head = blk;
     return blk;
 }
 
@@ -57,15 +73,28 @@ struct heap_blk *find_blk(size_t size) {
     return NULL;
 }
 
+struct heap_blk *extend_heap(size_t bytes) {
+    struct heap_blk *last = head;
+    while (last->next != NULL) last = last->next;
+
+    struct heap_blk *blk = (struct heap_blk *) sbrk(bytes);
+    blk->status = FREE;
+    blk->prev = last;
+    blk->size = bytes;
+    blk->next = NULL;
+    last->next = blk;
+    return blk;
+}
+
 void *alloc(size_t n, size_t size) {
     if (!head) 
         init_heap();
     size_t nbytes = n*size + BLK_STRUCT_SIZE;
-    printf("BYTES: %zu\n",n*size);
+    printf("BYTES: %zu\n\n",n*size);
     struct heap_blk *free_blk = find_blk(nbytes);
     if (!free_blk)
-        return NULL;
-    struct heap_blk *new_blk = split_chunk(head, nbytes);
+        free_blk = extend_heap(nbytes);
+    struct heap_blk *new_blk = split_chunk(free_blk, nbytes);
     print_blk();
     return PAYLOAD(new_blk);
 }
@@ -73,5 +102,7 @@ void *alloc(size_t n, size_t size) {
 
 
 void alloc_free(void *ptr) {
-    (void) ptr;
+    struct heap_blk *blk = BLK_HEADER(ptr);
+    blk->status = FREE;
+    //TODO: merge blocks??
 }
--- main.c
+++ main.c
@@ -10,8 +10,8 @@ typedef struct {
 int main() {
     person_t* personptr = (person_t *) alloc(1,sizeof(person_t));
     personptr->name = alloc(5,sizeof(char));
-    strcpy(personptr->name, "deni");
-    personptr->age = 10;
+    strcpy(personptr->name, "pedro");
+    personptr->age = 19;
     printf("%p \n",(void *) personptr);
     printf("%s %d\n", personptr->name, personptr->age);
     return 0;
